45bc5e5d55da1b583100683b13d35027b29b4d81
Neetu <cdc.psn@gmail.com>
2022-09-20 11:41:21 +0530

    committing finally now again

--- File1.py
+++ File1.py
@@ -6,3 +6,5 @@ for element in l1:
     if element%2==0:
         l2.append(element)
 print(l2,"is a list of even numbers")
+
+print('I have addee something new today on 20th sep')
--- File2.py
+++ File2.py
@@ -1,1 +1,2 @@
 print("Second file printed")
+print('i have added something new on 20th september')
