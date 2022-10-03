3d032f93fd85cf438ca6840bd7ab29ea2b801e08
Neetu <cdc.psn@gmail.com>
2022-10-03 13:09:15 +0530

    previous commit

--- File3.py
+++ File3.py
@@ -1,4 +1,21 @@
+s='betty bought some butter. the butter was bitter'
+'''
+l1=s.split(" ")
+print(l1)
 
-print("Third file printed")
+for index in range(len(l1)):
+    if l1[index]=='butter':
+        print(index)
+'''
+inb=s.rindex('bu')
+subs='butter'
+print(inb)
+print(inb+len(subs))
 
-print("trial written")
+
+
+
+
+
+
+
